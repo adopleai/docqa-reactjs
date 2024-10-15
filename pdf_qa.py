c6ad11c8858f2a9b03e5cdb0566b788fc12960d0
General <124350719+adopleai@users.noreply.github.com>
2024-10-15 19:33:29 +0530

    Update pdf_qa.py

--- pdf_qa.py
+++ pdf_qa.py
@@ -15,7 +15,6 @@ class Docqa:
         self.retriever = self._get_documents_knowledge_base(file_paths)
         self.llm = AzureChatOpenAI(
             azure_endpoint="https://azureadople.openai.azure.com/",
-            api_key="d40171e5187047b3928b8f3f63900e4a",
             api_version="2023-07-01-preview",azure_deployment="GPT-4o"
         )
 
